9b2e28c5f8a5b0869de6581c2352dfee846c6669
justanotherjess72 <101445909@georgebrown.ca>
2024-10-11 14:04:59 -0400

    udpate file structures

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const userRoutes = require('./routes/userRoutes'); 
-const employeeRoutes = require('./routes/employeeRoutes'); 
+const userRoutes = require('../routes/userRoutes'); 
+const employeeRoutes = require('../routes/employeeRoutes'); 
 require('dotenv').config(); 
 
 const app = express();
