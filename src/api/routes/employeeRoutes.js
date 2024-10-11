bf508b2b4e7fb993277a5db68b27498809140b01
justanotherjess72 <101445909@georgebrown.ca>
2024-10-11 14:02:45 -0400

    updated

--- src/api/routes/employeeRoutes.js
+++ src/api/routes/employeeRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const employeeController = require('../controllers/employeeController');
+const employeeController = require('..src/api/controllers/employeeController');
 
 const router = express.Router();
 
